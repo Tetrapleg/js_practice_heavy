3b0849b242ab2db7e4d6bff7182600795c89a8c7
Tetrapleg <rufmama@yandex.ru>
2020-06-08 17:35:11 +0300

    ДЗ №3 повышенной трудности

---
+++ script/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let number = 266219;
+
+number = String(number); //Преобразуем наше число в массив
+
+let x = 1; //Переменная для первого умножения
+
+for (let i = 0; i < number.length; i++) {
+  x *= number[i];
+} //Перемножаем цифры полученного массива
+
+console.log(x);
+
+x **= 3; //Возводим в степень
+
+x = String(x); //Преобразуем полученное число в строку
+
+console.log(x.substring(0, 2)); //Выводим первые две цифры полученного числа
+
+//2 способ
+number = 20076;
+let str = number + '';
+let arr = str.split('');
+let sum = arr.reduce(function(acc, item) {
+  return +item ? acc * item : acc;
+}, 1);
+console.log(sum);
+
+
+
+let lang = confirm('Для перехода на русский язык нажмите "ДА", для перехода на английский "НЕТ"');
+
+if (lang) {
+  console.log('Дни недели на русском');
+} else {
+  console.log('Дни недели на английском');
+}
+
+switch (lang) {
+  case true:
+    console.log('Дни недели на русском');
+    break;
+  default:
+    console.log('Дни недели на английском');
+}
+
+let arrLang = ['Дни недели на английском', 'Дни недели на русском'];
+
+console.log(arrLang[Number(lang)]);
+
+
+
+let namePerson = prompt('Введите Ваше имя');
+
+namePerson === 'Артем' ? console.log(namePerson + ' - директор') : 
+  namePerson === 'Максим' ? console.log(namePerson + ' - преподаватель') : 
+  console.log(namePerson + ' - студент');
+
