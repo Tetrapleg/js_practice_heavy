9495a0e1592740d9315f948be53292e24b89a34a
Tetrapleg <rufmama@yandex.ru>
2020-06-09 18:17:35 +0300

    Добавленен третий вариант через многомерный массив

--- script/script.js
+++ script/script.js
@@ -1,53 +1,31 @@
 'use strict';
 
-let number = 266219;
-
-number = String(number); //Преобразуем наше число в массив
-
-let x = 1; //Переменная для первого умножения
-
-for (let i = 0; i < number.length; i++) {
-  x *= number[i];
-} //Перемножаем цифры полученного массива
-
-console.log(x);
-
-x **= 3; //Возводим в степень
-
-x = String(x); //Преобразуем полученное число в строку
-
-console.log(x.substring(0, 2)); //Выводим первые две цифры полученного числа
-
-//2 способ
-number = 20076;
-let str = number + '';
-let arr = str.split('');
-let sum = arr.reduce(function(acc, item) {
-  return +item ? acc * item : acc;
-}, 1);
-console.log(sum);
-
-
 
 let lang = confirm('Для перехода на русский язык нажмите "ДА", для перехода на английский "НЕТ"');
+let rus = 'Дни недели на русском';
+let eng = 'Дни недели на английском';
 
 if (lang) {
-  console.log('Дни недели на русском');
+  console.log(rus);
 } else {
-  console.log('Дни недели на английском');
+  console.log(eng);
 }
 
 switch (lang) {
   case true:
-    console.log('Дни недели на русском');
+    console.log(rus);
     break;
   default:
-    console.log('Дни недели на английском');
+    console.log(eng);
 }
 
-let arrLang = ['Дни недели на английском', 'Дни недели на русском'];
+let arrLang = [eng, rus];
 
 console.log(arrLang[Number(lang)]);
+
+let arrMulti = [['Day on', 'Дни недели на'], ['english', 'русском']];
+console.log(arrMulti[0][Number(lang)] + ' ' + arrMulti[1][Number(lang)]);
+
 
 
 
