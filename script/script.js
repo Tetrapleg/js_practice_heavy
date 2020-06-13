3b2c21bd305ca7be4e2c98334f450c5b520a770f
Tetrapleg <rufmama@yandex.ru>
2020-06-13 19:36:33 +0300

    Создал функцию перебора

--- script/script.js
+++ script/script.js
@@ -3,11 +3,17 @@
 
 let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 
-document.write(week.join(', ') + ('</br>'));
 
-document.write(week.join('<br>') + ('</br>'));
 
-document.write(('<i>') + week.slice(-2).join(', ') + ('</i>') + ('</br>'));
 
-document.write(('<b>') + week[new Date().getDay()] + ('</b>'));
-
+for(let item in week){
+  if (item < 5 && item != new Date().getDay()) {
+    document.write(week[item] + ('</br>')); 
+  }
+  if (item > 4 && item != new Date().getDay()) {
+    document.write(('<em>') + week[item] + ('</em>') + ('</br>'));
+  }
+  if (item == new Date().getDay()) {
+  document.write(('<strong>') + week[new Date().getDay()] + ('</strong>'));
+  }
+}
