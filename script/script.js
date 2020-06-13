014732554352e08918de9f548cd0c771f3b9c860
Tetrapleg <rufmama@yandex.ru>
2020-06-13 16:01:39 +0300

    ДЗ № 7 усложнённое

--- script/script.js
+++ script/script.js
@@ -2,5 +2,12 @@
 
 
 let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-console.dir(new Date());
+
+document.write(week.join(', ') + ('</br>'));
+
+document.write(week.join('<br>') + ('</br>'));
+
+document.write(('<i>') + week.slice(-2).join(', ') + ('</i>') + ('</br>'));
+
+document.write(('<b>') + week[new Date().getDay()] + ('</b>'));
 
