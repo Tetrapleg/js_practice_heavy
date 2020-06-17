2481a7338cb078464772e70c728d153cdcb0d26b
Tetrapleg <rufmama@yandex.ru>
2020-06-17 11:51:46 +0300

    ввёл функции для склонения и добавления нуля

--- script/script.js
+++ script/script.js
@@ -1,22 +1,25 @@
 'use strict';
 
-let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let weekArr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-let month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+let monthArr = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
-document.write(('Сегодня ') + week[new Date().getDay()] + ', ');
-document.write(new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ');
-document.write(new Date().getFullYear() + ('года, '));
-let h = new Date().getHours();
-if (h === 1 || h === 21) {
-  document.write(new Date().getHours() + (' час ')); 
-}
-if (h === 2 || h === 3 || h === 4 || h === 22 || h === 23 || h === 24) {
-  document.write(new Date().getHours() + (' часа '));
-}
-document.write(new Date().getHours() + (' часаов '));
+document.write(('Сегодня ') + weekArr[new Date().getDay()] + ', ');
+document.write(new Date().getDate() + ' ' + monthArr[new Date().getMonth()] + ' ');
+document.write(new Date().getFullYear() + (' года, '));
+declinationOfHours (new Date().getHours());
 document.write(new Date().getMinutes() + (' минут '));
 document.write(new Date().getSeconds() + (' секунды'));
+
+function declinationOfHours (h) {
+  if (h === 1 || h === 21) {
+    document.write(h + (' час ')); 
+  }
+  if (h === 2 || h === 3 || h === 4 || h === 22 || h === 23 || h === 24) {
+    document.write(h + (' часа '));
+  }
+  document.write(h + (' часаов '));
+}
 
 document.write('</br>');
 document.write('</br>');
@@ -33,7 +36,7 @@ if (new Date().getMonth() >= 10 || new Date().getMonth() == 0) {
   document.write(('0') + new Date().getMonth() + '.')
 }
 document.write(new Date().getFullYear() + (' - '));
-if (h >= 10 || h !== 0) {
+if (new Date().getHours() >= 10 || new Date().getHours() !== 0) {
   document.write(new Date().getHours() + (':'));
 } else {
   document.write(('0') + new Date().getHours() + (':')); 
@@ -60,33 +63,23 @@ setInterval("calcDate()",1000);
 
 function calcDate() {
 
-    let date = new Date().getDate() + '.',
-        month = new Date().getMonth() + '.',
-        year = new Date().getFullYear() + (' - '),
-        hour = new Date().getHours() + (':'),
-        minutes = new Date().getMinutes() + (':'),
+    let date = new Date().getDate(),
+        month = new Date().getMonth(),
+        year = new Date().getFullYear(),
+        hour = new Date().getHours(),
+        minutes = new Date().getMinutes(),
         seconds = new Date().getSeconds();
 
-    if (date < 10) {
-      date = ('0') + new Date().getDate() + '.';
-    }
-    if (month < 10) {
-      month = ('0') + new Date().getMonth() + '.';
-    }
-    if (hour < 10) {
-      hour = ('0') + new Date().getHours() + (':'); 
+    function nullBeforeDate (a) {
+      if (a < 10) {
+        a = ('0') + a;
+      }
+      return a;
     }
 
-    if (minutes < 10) {
-      minutes = ('0') + new Date().getMinutes() + (':');
-    }
 
-    if (seconds < 10) {
-      seconds = ('0') + new Date().getSeconds();
-    }
-
-  //Выводим строку, содержащую дату и время в элемент с id="clock"
-   document.getElementById("date").innerHTML =  date + month + year + hour + minutes+ seconds;
+  //Выводим строку, содержащую дату и время в элемент с id="date"
+   document.getElementById("date").innerHTML =  nullBeforeDate(date) + '.' + nullBeforeDate(month) + '.' + nullBeforeDate(year) + (' - ') + nullBeforeDate(hour) + (':') + nullBeforeDate(minutes) + (':') + nullBeforeDate(seconds);
 }
 
 
